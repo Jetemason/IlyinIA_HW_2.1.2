8e085687f6f343c7e510b3f7378e1f96532ab9f6
Igor Ilyin <53910111+Jetemason@users.noreply.github.com>
2020-01-30 15:25:51 +0500

    Initial Commit

---
+++ IlyinIA_HW_2.1.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IlyinIA_HW_2.1.2
+//
+//  Created by Игорь Ильин on 30.01.2020.
+//  Copyright © 2020 Igor Ilyin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
